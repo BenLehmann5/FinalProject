68406d5aed676a324e6db059f91c39185bd37039
BenLehmann5 <92688639+BenLehmann5@users.noreply.github.com>
2021-11-10 12:03:40 -0600

    Add files via upload

---
+++ FinalProject.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+
+
+class GroceryList:
+    grocery = []
+
+    def create(self,shopping,item):
+        for i in shopping:
+            grocery.insert(END,i[0] + " - " + str(i[1]))
+
+    def Index(self,shopping,item):
+        ind = -1
+        for i in range(len(shopping)):
+            if shopping[i][0] == item:
+                ind = i
+        return ind
+
+    def removeLst(self,shopping,index):
+        del(shopping[index])
+
+
+
+
