4a056660950fc6b2f50b502062a2b1a4dc3c942a
Ben Lehmann <62218426+BenLehmann12@users.noreply.github.com>
2021-11-19 16:46:25 -0600

    Commit #2: Redo, the class and functions, use Queue and Stack. Added New Class with Map structure, added algorithm

--- FinalProject.py
+++ FinalProject.py
@@ -54,13 +54,57 @@ def UserItems():
             break
         grocery.adding(items)
     print(grocery.items)
+    addOr = input("Do you want to add another or delete? Enter 'Add' or 'Delete: ")
+    if(addOr == 'Add'):
+        added = input('Enter the item')
+        grocery.adding(added)
+        print(grocery.items)
+    elif(addOr == 'Delete'):
+        grocery.remove()
+        print(grocery.items)
+    return
 
-def userPicks():
-    user = input("Do you want to see the list of items or prices of the items?")
-    if user == 'P':
-        print(UserItems())
-    elif user =='F':
-        print("Hello")
+def UserCost():
+    cost = Prices()
+    i = 0
+    while 1:
+        i += 1
+        items = float(input('Enter Price %d: ' % i))
+        if items == 0:
+            break
+        cost.enqueue(items)
+    print(cost.queue)
+
+class UserAndCost:
+    def __init__(self):
+        self.dic = {}
+
+    def add(self,key,value):
+        self[key] = float(value)
+
+    def print_value(self):
+        for k,v in self.dic.items():
+            print(k + " : " + v)
+
+
+def combine():
+    cost_item = UserAndCost()
+    user_x = int(input("Enter the Number of items"))
+    for x in range(user_x):
+        item = input('Enter your item')
+        cost = input('Enter your price')
+        cost_item.add(item,cost)
+    cost_item.print_value()
+
+def InsertionSort(userList):
+    for i in range(1, len(userList)):
+        currentVal = userList[i]
+        pos = i
+        while pos>0 and userList[pos-1]>currentVal:
+            userList[pos]=userList[pos-1]
+            pos=pos-1
+        userList[pos] = currentVal
+
 
 if __name__ == "__main__":
-    print(userPicks())
+    print(combine())
